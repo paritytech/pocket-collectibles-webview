dce109fdf7d9f35f5646ac88583bc96e6bf2ef65
lovelaced <erin@parity.io>
2026-08-06 12:35:00 +0100

    change endpoint

--- src/collectibles/resolver.ts
+++ src/collectibles/resolver.ts
@@ -20,8 +20,8 @@
 
 import cidMap from './cid_map.json'
 
-/** Web3 Summit IPFS gateway. Serves catalogue images at `/ipfs/<cid>`. */
-const IPFS_GATEWAY = 'https://summit-ipfs.polkadot.io/ipfs'
++/** Paseo Bulletin Next IPFS gateway. Serves catalogue images at `/ipfs/<cid>`. */
++const IPFS_GATEWAY = 'https://paseo-bulletin-next-ipfs.polkadot.io/ipfs'
 
 // Rarity-roll bands over the uint16 space (0..65535), read from bytes 0-1:
 //   [0, RARE_THRESHOLD) → rare pool
